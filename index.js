690eb7808eb3a5b8b18eb9015df02803fe8b9cb9
ElenaYF <128487747+ElenaYF@users.noreply.github.com>
2023-05-28 20:43:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,97 +1,559 @@
-#!/usr/bin/env node
+// Підключаємо технологію express для back-end сервера
+const express = require('express')
+// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
+const router = express.Router()
 
-/**
- * Module dependencies.
- */
+//=================================================================
 
-const app = require('./app')
-const debug = require('debug')(
-  'template-express-live-reload:server',
-)
-const http = require('http')
-
-/**
- * Get port from environment and store in Express.
- */
-
-const port = normalizePort(process.env.PORT || '3000')
-app.set('port', port)
-
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app)
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port)
-server.on('error', onError)
-server.on('listening', onListening)
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  const port = parseInt(val, 10)
-
-  if (isNaN(port)) {
-    // named pipe
-    return val
-  }
-
-  if (port >= 0) {
-    // port number
-    return port
-  }
-
-  return false
+var  header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov', 
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ на місяць',
+  adrress: '47398279, str. Sumskaya 44, Kiev, Ukraine',
 }
 
-/**
- * Event listener for HTTP server "error" event.
- */
+var  footer = {
+  social: {
+    email: {
+      text: 'ivanov@mail.com',
+      href: 'mailto:ivanov@mail.com',
+    },
 
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error
-  }
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
 
-  const bind =
-    typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.log(bind + ' requires elevated privileges')
-      process.exit(1)
-      break
-    case 'EADDRINUSE':
-      console.log(bind + ' is already in use')
-      process.exit(1)
-      break
-    default:
-      throw error
-  }
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
 }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
 
-function onListening() {
-  const addr = server.address()
-  const bind =
-    typeof addr === 'string'
-      ? 'pipe ' + addr
-      : 'port ' + addr.port
-  debug('Listening on ' + bind)
-  console.log(
-    'Listening on ' + 'http://localhost:' + addr.port,
-  )
-}
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('index', {})
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/summary', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('summary', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume| Summary',
+    },
+    header,
+   
+
+    
+    
+    main: {
+      summary: {
+        title: 'Summary',
+        text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.'
+      },
+  
+      experience: {
+        title: 'Other experience',
+        text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.'
+      },
+    },
+
+    footer,
+
+  })
+})
+
+// ================================================================
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+    
+    main: {
+     skills: [
+     {
+      name: 'HTMl',
+      point: (10),
+      isTop: true,
+     },
+     {
+      name: 'Handlebars',
+      point: (8),
+      isTop: true,
+     },
+     {
+      name: 'VS code & NPM',
+      point: (9),
+      isTop: false,
+     },
+     {
+      name: 'React.js',
+      point: (0),
+     },
+     {
+      name: 'PHP',
+      point: null,
+     },
+     ],
+
+     hobbies: [
+      {name: 'fishing',
+      isMain: false,
+      },
+      {name: 'dancing',
+      isMain: true,
+      },
+      {name: 'jogging',
+      isMain: true,
+      },
+      
+     ],
+    },
+
+    footer,
+
+  })
+})
+
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+    
+    main: {
+      educations: [
+
+        { name: 'School',
+          isEnd: true,
+        },
+        { name: 'Universaty1',
+          isEnd: false,
+        },
+        { name: 'Universaty2',
+          isEnd: true,
+        },
+        { name: 'IT-BRAINS',
+          isEnd: false,
+        },
+      ],
+      certificates: [
+        { name: 'school leaving certificate',
+          id: (12),
+        },
+        { name: 'diploma',
+          id:(5),
+        },
+        { name: 'certificate',
+          id: (0),
+        },
+      ]
+        },
+
+    footer,
+
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+   
+   layout: 'big',
+   
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+    
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company:{
+              name:'IT Brains',
+              url:'https://it-brains.com.ua/',
+        },
+        duration: {
+          from: '10.10.2022',
+          to: null,
+        },
+        projectAmount: 3,
+
+        projects:  [
+          {
+            name:'Resume',
+            url: 'https://resume.com.ua/',
+            about: 'Purpose of writing a resume.',
+            stacks: [
+               {
+                name: 'React.js',
+               },
+               {
+                name: 'HTML / CSS',
+               },
+               {
+                name: 'Node.js',
+               },
+            ],
+            stackAmount: 7,
+           
+
+            awards: [
+              {
+                name: "Promotion"
+              },
+              {
+                name: "Travel to overseas office"
+              },
+            ],
+             awardAmount: 4,
+          },
+        ],
+        },
+      ],
+        },
+
+    footer,
+
+  })
+})
+
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/person', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('person', {
+    layout: 'person',
+   
+    persons: {
+      name: 'Emma Johnson',
+      age: 32,
+      gender: 'Female',
+      address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY',
+        zip: '10001',
+        country: 'USA',
+      },
+      education: [
+        {
+          degree: 'Bachelor of Science',
+          major: 'Computer Science',
+          university:
+            'Massachusetts Institute of Technology',
+          graduationYear: 2012,
+        },
+      ],
+      workExperience: [
+        {
+          company: 'Google',
+          title: 'Software Engineer',
+          startDate: '2012-06-01',
+          endDate: '2016-12-31',
+          responsibilities: [
+            'Developed new features for Google Maps',
+            'Worked on improving search algorithms',
+          ],
+          year_founded: 1990,
+          industry: 'Technology',
+          employees: [
+            {
+              name: 'John Smith',
+              position: 'CEO',
+              department: 'Executive',
+              projects: [
+                {
+                  name: 'Project Alpha',
+                  description:
+                    'Developing new software platform',
+                  status: 'In Progress',
+                  teams: [
+                    {
+                      team_name: 'Awesome Team',
+                      team_leader: {
+                        name: 'John Smith',
+                        title: 'Team Leader',
+                        email: 'john.smith@example.com',
+                      },
+                      team_members: [
+                        {
+                          name: 'Alice Johnson',
+                          title: 'Software Engineer',
+                          email:
+                            'alice.johnson@example.com',
+                          skills: ['Java', 'Python', 'SQL'],
+                          projects: [
+                            {
+                              name: 'Project A',
+                              description:
+                                'Lorem ipsum dolor sit amet',
+                              technologies: [
+                                'Java',
+                                'Spring Framework',
+                              ],
+                              team_members: [
+                                {
+                                  name: 'Bob Lee',
+                                  title:
+                                    'Software Engineer',
+                                },
+                                {
+                                  name: 'Cindy Chen',
+                                  title: 'UI Designer',
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  })
+})
+
+// ================================================================
+
+// ================================================================
+
+router.get('/program', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('program', {
+
+    layout: 'program',
+    
+    program: {
+      excursion: {
+        name: 'Cultural Tour',
+        location: {
+          city: 'Paris',
+          country: 'France',
+        },
+        date: '2023-06-15',
+        program: [
+          {
+            name: 'Museum Visit',
+            type: 'Art',
+            duration: 3,
+            details: {
+              museum: {
+                name: 'The Louvre',
+                location: {
+                  address: 'Rue de Rivoli',
+                  city: 'Paris',
+                  country: 'France',
+                },
+                exhibits: [
+                  {
+                    name: 'Mona Lisa',
+                    artist: 'Leonardo da Vinci',
+                    description: 'Iconic portrait painting',
+                    audio_guide: true,
+                  },
+                  {
+                    name: 'Winged Victory of Samothrace',
+                    artist: null,
+                    description:
+                      'Ancient Greek statue of Nike, the goddess of victory',
+                    audio_guide: true,
+                  },
+                ],
+              },
+              guide: {
+                name: 'Francois',
+                language: 'French',
+                rating: 4.8,
+              },
+            },
+          },
+          {
+            name: 'Cultural Show',
+            type: 'Music and Dance',
+            duration: 2,
+            details: {
+              venue: {
+                name: 'Moulin Rouge',
+                location: {
+                  address: '82 Boulevard de Clichy',
+                  city: 'Paris',
+                  country: 'France',
+                },
+              },
+              performers: [
+                {
+                  name: 'Mireille Mathieu',
+                  type: 'Chanson singer',
+                },
+                {
+                  name: "Ballet de l'Opéra National de Paris",
+                  type: 'Classical ballet company',
+                },
+              ],
+              guide: {
+                name: 'Sophie',
+                language: 'English',
+                rating: 4.6,
+              },
+            },
+          },
+        ],
+      },
+    },
+  })
+})
+
+// ================================================================
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/bio', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('bio', {
+
+    layout: 'bio',
+
+    name: 'Albert Einstein',
+    birthdate: 'March 14, 1879',
+    birthplace:
+      'Ulm, Kingdom of Württemberg, German Empire',
+    deathdate: 'April 18, 1955',
+    deathplace: 'Princeton, New Jersey, United States',
+    nationality: 'Swiss, German, American',
+    occupation: 'Theoretical Physicist',
+    known_for: [
+      'Theory of Relativity',
+      'E=mc²',
+      'Photoelectric Effect',
+      'Brownian Motion',
+    ],
+    education: [
+      {
+        degree: 'Doctor of Philosophy',
+        field: 'Physics',
+        institution: 'University of Zurich',
+        year: 1905,
+      },
+    ],
+    notable_publications: [
+      {
+        title: 'On the Electrodynamics of Moving Bodies',
+        year: 1905,
+        publisher: 'Annalen der Physik',
+      },
+      {
+        title:
+          'Does the Inertia of a Body Depend Upon Its Energy Content?',
+        year: 1905,
+        publisher: 'Annalen der Physik',
+      },
+    ],
+    partners: [
+      {
+        name: 'Mileva Marić',
+        relationship: 'First wife',
+        years: '1903-1919',
+      },
+      {
+        name: 'Elsa Einstein',
+        relationship:
+          "Second wife, also Einstein's first cousin",
+        years: '1919-1936',
+      },
+    ],
+    awards: [
+      {
+        title: 'Nobel Prize in Physics',
+        year: 1921,
+        description:
+          'Awarded for his explanation of the photoelectric effect',
+      },
+    ],
+    influences: [
+      'Isaac Newton',
+      'James Clerk Maxwell',
+      'Hermann Minkowski',
+    ],
+    influenced: [
+      'Niels Bohr',
+      'Erwin Schrödinger',
+      'Werner Heisenberg',
+      'Richard Feynman',
+    ],
+    quotes: [
+      'Imagination is more important than knowledge.',
+      'I have no special talent. I am only passionately curious.',
+      'The important thing is not to stop questioning.',
+      'The most beautiful thing we can experience is the mysterious. It is the source of all true art and all science.',
+    ],
+    major_discoveries: [
+      {
+        title: 'Photoelectric Effect',
+        year: 1905,
+        description:
+          'Einstein explained the photoelectric effect, where electrons are emitted from a metal surface when it is illuminated by light.',
+      },
+    ],
+    contributions: {
+      title: 'Inventions',
+      description:
+        'Leonardo designed and invented a wide variety of machines and devices, including a helicopter, a diving suit, and a self-propelled cart. Many of his inventions were centuries ahead of their time.',
+      year: 'Late 15th to early 16th century',
+      field: 'Invention',
+    },
+  })
+})
+
+// ================================================================
+
+
+// Підключаємо роутер до бек-енду
+module.exports = router
